1d11e1139dd8233413551b845047c0325b29e8e2
VikingHesse <70264379+VikingHesse@users.noreply.github.com>
2020-08-28 09:37:35 +0200

    introduktionskod

--- README.md
+++
@@ -1,5 +0,0 @@
-# viking_hesse_TE19C_programmering1
-
-Programmering-1 kurs
-
-test
---
+++ intrakod.syntax/syntax.py
@@ -0,0 +1,10 @@
+print("Hej")
+
+name = "Viking"
+
+print(f"Hej {name}")
+
+side = float(input("Kvadratens sida: "))
+area = side**2
+
+print(area)
